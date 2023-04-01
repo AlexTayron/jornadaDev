48e1286010c6c19e7569cb22203c41572c91ca91
Alex Tayron <57293582+AlexTayron@users.noreply.github.com>
2023-03-31 23:09:25 -0300

    finalização da parte front end do projeto

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={100}
+          messages={220}
+          shares={340}
+          name="Jose"
+          description="Gente boa"
+          music="patience - Guns 'n roses"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes={555}
+          messages={654}
+          shares={1}
+          name="Pedro"
+          description="Gato Massa"
+          music="Pedra Leticia"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
